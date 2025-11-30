77ccda1bc1578a8fd4c76918a16b170c306769f7
GitInno <86991526+gitnnolabs@users.noreply.github.com>
2025-11-30 10:48:13 -0300

    Torna a execução das tarefas de marcação obrigatório o uso do Llama.

--- config/settings/base.py
+++ config/settings/base.py
@@ -299,7 +299,7 @@ SIMPLE_JWT = {
 }
 
 # LLAMA
-LLAMA_ENABLED = env.bool("LLAMA_ENABLED", default=False)
+LLAMA_ENABLED = env.bool("LLAMA_ENABLED", default=True)
 MODEL_LLAMA = "llama-3.2-3b-instruct-q4_k_m.gguf"
 
 #Aumento en el límite de campos
